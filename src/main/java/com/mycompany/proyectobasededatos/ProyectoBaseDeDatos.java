4cbae0521e881a1e8a1fb6399ade1ed6dec31a46
varga <varga@192.168.1.49>
2023-02-11 21:15:58 -0600

    FirstCommit

---
+++ src/main/java/com/mycompany/proyectobasededatos/ProyectoBaseDeDatos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectobasededatos;
+
+/**
+ *
+ * @author varga
+ */
+public class ProyectoBaseDeDatos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
