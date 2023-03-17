30ba98fa5e6d6c8f8d08e4d00ca97e994e8b4336
varga <varga@192.168.1.49>
2023-03-16 21:01:31 -0600

    Test

---
+++ ProyectoHospital/src/main/java/com/mycompany/proyectohospital/ProyectoHospital.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectohospital;
+
+/**
+ *
+ * @author varga
+ */
+public class ProyectoHospital {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+                System.out.println("World!");
+
+    }
+}
